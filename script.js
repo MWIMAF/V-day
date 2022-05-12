22bea92e3192418b18233e57a87b6342731574f9
Wima <79455703+MWIMAF@users.noreply.github.com>
2022-05-12 23:29:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function myFunc()
+  {
+    document.getElementById("video").src="https://i1.sndcdn.com/avatars-000444893235-f8vp4q-t500x500.jpg";
+    document.getElementById("text").innerHTML = "Ini clue hehe";
+  }
